dc7d7be40bd5513d225c6dea1fdfb9f2771829e5
mazharul <rafi1998.bdr@gmail.com>
2024-11-23 21:32:24 +0600

    module 2 complete

--- js-begening/basics-01-starting-project/assets/scripts/app.js
+++ js-begening/basics-01-starting-project/assets/scripts/app.js
@@ -1,6 +1,6 @@
 const defaultResult = 0;
-
 let currentResult = defaultResult;
+let logEntries = [];
 
 addBtn.addEventListener('click', add)
 subtractBtn.addEventListener('click', substact)
@@ -12,58 +12,53 @@ function getUserInput() {
     return parseInt(userInput.value);
 }
 
+// Generate and writes calculation log
 function add() {
     const enteredNumber = getUserInput();
-
-    const calcDescription = `${currentResult} + ${enteredNumber}`;
     const initialResult = currentResult;
-
     currentResult += enteredNumber;
-
-    createAndWriteOutput('+', initialResult, enteredNumber);
+    createAndWriteOutput('+', initialResult, enteredNumber); // from vendor file
+    writeToLog('ADD', initialResult, enteredNumber, currentResult)
 }
 
 function substact() {
     const enteredNumber = getUserInput();
-
-    const calcDescription = `${currentResult} - ${enteredNumber}`;
-
     const initialResult = currentResult;
-
-
     currentResult -= enteredNumber;
-
     createAndWriteOutput('-', initialResult, enteredNumber);
+    writeToLog('SUBTRACT', initialResult, enteredNumber, currentResult)
+
 }
 
 function multiply() {
     const enteredNumber = getUserInput();
-
-    const calcDescription = `${currentResult} * ${enteredNumber}`;
-
     const initialResult = currentResult;
-
-
     currentResult *= enteredNumber;
-
     createAndWriteOutput('*', initialResult, enteredNumber);
+    writeToLog('MULTIPLY', initialResult, enteredNumber, currentResult)
 }
 
 function division() {
     const enteredNumber = getUserInput();
-
     const initialResult = currentResult;
-
-
-    const calcDescription = `${currentResult} / ${enteredNumber}`;
-
     currentResult /= enteredNumber;
-
     createAndWriteOutput('/', initialResult, enteredNumber);
+    writeToLog('DIVISION', initialResult, enteredNumber, currentResult)
 }
 
 function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
     const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`
     outputResult(currentResult, calcDescription)
+}
+
+function writeToLog(operation, prevResult, number, result) {
+    const logEntry = {
+        operation: operation,
+        prevResult: prevResult,
+        number: number,
+        result: result
+    }
+    logEntries.push(logEntry);
+    console.log(logEntries)
 }
 
