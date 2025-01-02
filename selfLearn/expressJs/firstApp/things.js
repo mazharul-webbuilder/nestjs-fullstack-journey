910f94b4cc454f5c12b5b61677e355c246ed1e25
Mazharul <mazharul.jatri@gmail.com>
2025-01-02 16:59:59 +0600

    first express

--- selfLearn/expressJs/firstApp/things.js
+++ selfLearn/expressJs/firstApp/things.js
@@ -1,3 +1,8 @@
 const express = require('express');
-const app = express();
+const router = express.Router();
 
+router.get('/test', (req, res) => {
+    res.send('Hello World!');
+})
+
+module.exports = router;
