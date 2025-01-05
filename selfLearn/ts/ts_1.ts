ee1e52dd581dc0589a4e888c2b818afa882cf0a2
Mazharul <mazharul.jatri@gmail.com>
2025-01-05 16:16:08 +0600

    nestJs

--- selfLearn/ts/ts_1.ts
+++ selfLearn/ts/ts_1.ts
@@ -4,7 +4,7 @@ var sonAge = 0.2;
 var isHealthy = true;
 // Function with type
 function printBabyInfo() {
-    console.log("Baby Name: ".concat(sonName, ". Age: ").concat(sonAge));
+    console.log("Baby Name: ".concat(sonName, ". Age: ").concat(String(sonAge)));
     return true;
 }
 printBabyInfo();
