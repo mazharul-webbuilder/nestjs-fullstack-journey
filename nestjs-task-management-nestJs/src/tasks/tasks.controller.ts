cbf174b0fe137cfc4f68403bd388837a6385a1a0
Mazharul <mazharul.jatri@gmail.com>
2025-01-06 15:09:20 +0600

    validation

--- nestjs-task-management-nestJs/src/tasks/tasks.controller.ts
+++ nestjs-task-management-nestJs/src/tasks/tasks.controller.ts
@@ -1,4 +1,15 @@
-import { Body, Controller, Delete, Get, NotFoundException, Param, Patch, Post, Query } from '@nestjs/common';
+import {
+    Body,
+    Controller,
+    Delete,
+    Get,
+    NotFoundException,
+    Param,
+    Patch,
+    Post,
+    Query, UsePipes,
+    ValidationPipe,
+} from '@nestjs/common';
 import {TasksService} from "./tasks.service";
 import { Task, TaskStatus } from './task.model';
 import {CreateTaskDto} from "./dto/create-task.dto";
@@ -35,6 +46,7 @@ export class TasksController {
     }
 
     @Post()
+    @UsePipes(ValidationPipe)
     createTask(@Body() createTaskDto: CreateTaskDto): Task {
         return this.taskService.createTask(createTaskDto)
     }
